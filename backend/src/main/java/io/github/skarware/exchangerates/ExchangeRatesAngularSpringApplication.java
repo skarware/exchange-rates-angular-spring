bde1096e2c8a4572cd4b986ed40eb8dbb7d18477
Martynas Skar <skarware@gmail.com>
2020-07-19 23:23:29 +0300

    Initial commit

---
+++ backend/src/main/java/io/github/skarware/exchangerates/ExchangeRatesAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.skarware.exchangerates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesAngularSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesAngularSpringApplication.class, args);
+    }
+
+}
